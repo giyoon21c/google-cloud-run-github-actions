73ccf3538e0b9ec1eab68ef767b3089faa497580
Giyoung Yoon <giyoon@verily.com>
2022-12-26 16:59:45 -0800

    Spaces...

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    name = request.args.get('name', 'World')
-    return f'Hello {name}!'
+  name = request.args.get('name', 'World')
+  return f'Hello {name}!'
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+  app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
